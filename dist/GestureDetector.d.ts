0fb676da7a7cd53de110a1e6b01add1a670c3198
David Valero <dvdvalero89@gmail.com>
2021-01-07 20:08:56 +0100

    Added longpress to detector

--- dist/GestureDetector.d.ts
+++ dist/GestureDetector.d.ts
@@ -1,6 +1,7 @@
 import { SwipeCallback, SwipeConfig } from "./Swipe";
 import { DoubleTapCallback, DoubleTapConfig } from "./DoubleTap";
 import { SlideCallback } from "./Slide";
+import { LongPressCallback } from "./LongPress";
 export interface GestureConfig {
 }
 export declare class GestureDetector {
@@ -8,10 +9,12 @@ export declare class GestureDetector {
     private swipe;
     private doubleTap;
     private slide;
+    private longPress;
     private callbacks;
     constructor(element: string | Element, cfg?: GestureConfig);
     onSwipe(cb: SwipeCallback, cfg?: SwipeConfig): this;
     onDoubleTap(cb: DoubleTapCallback, cfg?: DoubleTapConfig): this;
     onSlide(cb: SlideCallback): this;
+    onLongPress(cb: LongPressCallback): this;
     private _addListener;
 }
