925a87486df887f7283fc1cb47a5b274a35cce84
David Valero <dvdvalero89@gmail.com>
2021-01-07 11:37:10 +0100

    Removed comments

--- dist/Slide.js
+++ dist/Slide.js
@@ -28,9 +28,7 @@ var Slide = /** @class */ (function () {
         return data;
     };
     Slide.prototype._onMouseMove = function (evt) {
-        console.log(this.lastX + "-" + evt.screenX + " " + this.lastY + "-" + evt.screenY);
         if (this.lastX === evt.screenX && this.lastY === evt.screenY) {
-            console.log('Skip');
             return;
         }
         this.lastX = evt.screenX;
