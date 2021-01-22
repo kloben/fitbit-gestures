92601f9c92d87eb7eb129b198cc214c248122c27
David Valero <dvdvalero89@gmail.com>
2021-01-22 17:42:36 +0100

    Minor fix for missing MouseEvent definitions

--- __tests__/test.util.ts
+++ __tests__/test.util.ts
@@ -1,3 +1,14 @@
+interface MouseEvent {
+  readonly screenX: number;
+  readonly screenY: number;
+  readonly defaultPrevented: boolean;
+  readonly target: any;
+  readonly type: string;
+  preventDefault(): void;
+  stopImmediatePropagation(): void;
+  stopPropagation(): void;
+}
+
 export class TestUtil {
   static getEvent (x: number, y: number): MouseEvent {
     return {
