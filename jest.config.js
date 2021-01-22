792ff122180cc8f608ae63238593c080d3bb53a6
David Valero <dvdvalero89@gmail.com>
2021-01-22 17:35:27 +0100

    Fixed coverage detector

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
   testMatch: ['**/__tests__/**/*.spec.ts?(x)']
 }
