958fcc9b774eecbd6a326bdeddae695aac84684c
David Valero <dvdvalero89@gmail.com>
2021-01-25 20:57:08 +0100

    Add gesture detector to coverage files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: false,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/gestures/**/*.{js,ts}', 'src/helpers/**/*.{js,ts}'],
+  collectCoverageFrom: ['src/gestures/**/*.{js,ts}', 'src/helpers/**/*.{js,ts}', 'src/GestureDetector.ts'],
   testMatch: ['**/__tests__/**/*.spec.ts?(x)']
 }
